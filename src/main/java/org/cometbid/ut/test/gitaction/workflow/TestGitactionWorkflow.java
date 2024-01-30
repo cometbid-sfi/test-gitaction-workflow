3b950f4e6747ccbec3492665a13ac846a91be9e9
Oluwagbemiga Adebowale <gbenga_java@yahoo.com>
2024-01-30 09:49:22 -0500

    pushing first changes

---
+++ src/main/java/org/cometbid/ut/test/gitaction/workflow/TestGitactionWorkflow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.cometbid.ut.test.gitaction.workflow;
+
+/**
+ *
+ * @author samueladebowale
+ */
+public class TestGitactionWorkflow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
